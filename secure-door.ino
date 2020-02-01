a053eb8ee3d09ee9287a1dc31957dbbdd2b2386d
spike322 <m.borrazo@studenti.unisa.it>
2020-02-01 17:30:49 +0100

    Add user adding and menu selection

--- secure-door.ino
+++ secure-door.ino
@@ -11,10 +11,18 @@
 #define RST_PIN 9
 #define ALLOWED_LED 4
 #define NOT_ALLOWED_LED 5
+#define UP 1
+#define DOWN 2
+#define CHECK 3
 
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
+String options[] = {"Approximate your card to the reader...","Add user...","Delete user..."};
 int status = WL_IDLE_STATUS;
+int choice = 0;
+String uid = "";
+String sPayload;
+char* cPayload;
 
 /*************** TEMPORARY SIMULATION ***************/
 const int deniedButton = 8;
@@ -26,12 +34,10 @@ const char broker[] = MQTT_BROKER;
 const int port = MQTT_BROKER_PORT;
 const char username[] = MQTT_BROKER_USER;
 const char password[] = MQTT_BROKER_PWD;
-const char access_allowed[] = "access/allowed";
-const char access_denied[] = "access/denied";
-const char sender_topic[] = "access/request";
-const char register_new[] = "register/new";
-const char register_allowed[] = "register/allowed";
-const char register_denied[] = "register/denied";
+const char access_request[] = "access/request";
+const char access_response[] = "access/response";
+const char register_request[] = "register/request";
+const char register_response[] = "register/response";
 
 Servo servo;
 MFRC522 mfrc522(SS_PIN, RST_PIN); 
@@ -43,7 +49,7 @@ MqttClient mqttClient(wifiClient);
 unsigned long lastMillis = 0;
 
 void openDoorRequest(String addressCard) {
-  mqttClient.beginMessage(sender_topic);
+  mqttClient.beginMessage(access_request);
   mqttClient.print((String) addressCard);
   mqttClient.endMessage();
 }
@@ -94,23 +100,37 @@ void mqttBrokerConnect() {
   // subscribe to topics
   Serial.println("Subscribing to topics: ");
   Serial.print(" - ");
-  Serial.println(sender_topic);
-  mqttClient.subscribe(sender_topic);
+  Serial.println(access_request);
+  mqttClient.subscribe(access_request);
   Serial.print(" - ");
-  Serial.println(access_allowed);
-  mqttClient.subscribe(access_allowed);
+  Serial.println(access_response);
+  mqttClient.subscribe(access_response);
   Serial.print(" - ");
-  Serial.println(access_denied);
-  mqttClient.subscribe(access_denied);
+  Serial.println(register_request);
+  mqttClient.subscribe(register_request);
+  Serial.print(" - ");
+  Serial.println(register_response);
+  mqttClient.subscribe(register_response);
 }
 
 // On receive message from the topic
 void onMqttMessage(int messageSize) {
-  if (mqttClient.messageTopic() == access_allowed) {
-    openDoor();
-  } else if (mqttClient.messageTopic() == access_denied) {
-    denyAccess();
+  String topic = (String) mqttClient.messageTopic();
+  String s = "";
+  while (mqttClient.available()) {
+    s.concat((char)mqttClient.read());
   }
+  if (topic == access_response) {
+    if (s == "success") {
+      openDoor();
+    } else {
+      denyAccess();
+    }
+  } else if (topic == register_response) {
+    Serial.println(s);
+  }
+  
+  Serial.println(options[choice]);
 } 
 
 void wifiConnect() {
@@ -183,38 +203,9 @@ void printMacAddress(byte mac[]) {
   Serial.println();
 }
 
-void setup() {
-  pinMode(ALLOWED_LED, OUTPUT);
-  pinMode(NOT_ALLOWED_LED, OUTPUT);
-
-  // TEMPORARY SIMULATION
-  pinMode(allowedButton, INPUT);
-  pinMode(deniedButton, INPUT);
-
-  lcd.begin();
-  lcd.backlight();
-  lcd.clear();
-  lcd.print("Waiting for a card...");
-  servo.attach(2);
-  servo.write(0);
-  Serial.begin(9600);  
-  while (!Serial);
-  wifiConnect();
-  mqttBrokerConnect();
-  SPI.begin();
-  mfrc522.PCD_Init();  
-  Serial.println("Approximate your card to the reader...");
-  Serial.println();
-}
-
-void loop() {
-  mqttClient.poll();
-  
-  digitalWrite(ALLOWED_LED, LOW);
-  digitalWrite(NOT_ALLOWED_LED, LOW);
-
+String readCard() {
   // Look for new cards
-  /*if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
     return;
   }
@@ -237,29 +228,150 @@ void loop() {
   
   Serial.println();
   Serial.print("Message : ");
-  content.toUpperCase();*/
+  content.toUpperCase();
+  return (String) content;
+}
 
+
+String readCardWaiting() {
+  // Look for new cards
+  while ( ! mfrc522.PICC_IsNewCardPresent());
+  // Select one of the cards
+  while ( ! mfrc522.PICC_ReadCardSerial());
+  String content= "";
+  byte letter;
+  
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  
+  content.toUpperCase();
+  return (String) content;
+}
+
+void addCard(String newCard) {
+  Serial.print("New card UID: ");
+  Serial.println(newCard);
+  Serial.println();
+  Serial.println("Confirm with administrator card...");
+  String admin = "E9 33 97 47";//readCardWaiting();
+    
+  mqttClient.beginMessage(register_request);
+  mqttClient.print((String) "{ \"newCard\": \""+ newCard + "\", \"admin\": \""+ admin + "\" }");
+  mqttClient.endMessage();
+  
+  choice = 0;
+}
+
+void setup() {
+  pinMode(ALLOWED_LED, OUTPUT);
+  pinMode(NOT_ALLOWED_LED, OUTPUT);
+  pinMode(CHECK, INPUT);
+  pinMode(UP, INPUT);
+  pinMode(DOWN, INPUT);
+
+  // TEMPORARY SIMULATION
+  pinMode(allowedButton, INPUT);
+  pinMode(deniedButton, INPUT);
+
+  lcd.begin();
+  lcd.backlight();
+  lcd.clear();
+  lcd.print("Waiting for a card...");
+  servo.attach(2);
+  servo.write(0);
+  Serial.begin(9600);  
+  while (!Serial);
+  wifiConnect();
+  mqttBrokerConnect();
+  SPI.begin();
+  mfrc522.PCD_Init();  
+  Serial.println(options[choice]);
+}
+
+void loop() {
+  mqttClient.poll();
+  
+  digitalWrite(ALLOWED_LED, LOW);
+  digitalWrite(NOT_ALLOWED_LED, LOW);
+
+  if (digitalRead(UP) == 1) { //INCREASE CHOICE
+    choice = (choice + 1) % 3;
+    Serial.println(options[choice]);
+  } else if(digitalRead(DOWN) == 1) { //DECREASE CHOICE
+    if (choice > 0) {
+      choice = (choice - 1) % 3;
+      Serial.println(options[choice]);
+    }
+  }
+
+  delay(150);
+
+  /*switch(choice) {
+    case 0:
+      Serial.println("Approximate your card to the reader...");
+      Serial.println();
+      break;
+    case 1:
+      Serial.println("Add user card...");
+      Serial.println();
+      break;
+    case 2:
+      Serial.println("Delete user card...");
+      Serial.println();
+      break;
+    default: break;
+  }*/
+
+  // Look for new cards
+  /*if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    return;
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String uid= "";
+  byte letter;
+  
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     uid.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     uid.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  uid.toUpperCase();*/
+
+  /*************** TEMPORARY SIMULATION ***************/
   if(digitalRead(allowedButton) == 1) {
-    openDoorRequest("17 0E 70 1C");//content.substring(1)
+    uid = "17 0E 70 1C";
     Serial.println("Green pressed!");
   }
   if(digitalRead(deniedButton) == 1) {
-    openDoorRequest("E9 33 97 47");
+    uid = "AA BB CC DD";
     Serial.println("Red pressed!");
   }
-  
-  /*if (content.substring(1) == "17 0E 70 1C") {
-    lcd.clear();
-    Serial.println("Authorized access");
-    Serial.println();
-    openDoorRequest(content.substring(1));
-  } else {
-    lcd.clear();
-    lcd.print("Not allowed!");
-    Serial.println("Access denied");
-    Serial.println();
-    digitalWrite(NOT_ALLOWED_LED, HIGH);
-    delay(1000);
-  }*/
-  delay(500);
+  /****************************************************/
+
+  if(uid != "") {
+    switch(choice) {
+      case 0:
+        openDoorRequest(uid);
+        break;
+      case 1:
+        addCard(uid);
+        break;
+      case 2:
+        //removeCard(uid);
+        break;
+      default: break;
+    }
+  }
+
+  uid = "";
 }
