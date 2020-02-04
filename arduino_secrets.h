fcbd0d2a304f71427fd6538eb969c0aa0bdc2bee
spike322 <m.borrazo@studenti.unisa.it>
2020-02-04 13:14:23 +0100

    Not working for now

--- arduino_secrets.h
+++ arduino_secrets.h
@@ -1,5 +1,5 @@
-#define SECRET_SSID "TP-LINK_4DDA46"//"Tu Wi-Fi lamericano"
-#define SECRET_PASS "p4ssw0rdr0ut3rTPL1NK"//"751bbq?yi8r"
+#define SECRET_SSID "Tu Wi-Fi lamericano"
+#define SECRET_PASS "751bbq?yi8r"
 #define MQTT_BROKER "tailor.cloudmqtt.com"
 #define MQTT_BROKER_PWD "z7YL9QScgIuL"
 #define MQTT_BROKER_PORT 12489
--- secure-door.ino
+++ secure-door.ino
@@ -11,23 +11,20 @@
 #define RST_PIN 9
 #define ALLOWED_LED 4
 #define NOT_ALLOWED_LED 5
-#define UP 1
-#define DOWN 2
+#define UP 13
+#define DOWN 12
 #define CHECK 3
 
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
-String options[] = {"Approximate your card to the reader...","Add user...","Delete user..."};
+String options[] = {"       ENTER","      ADD USER","    DELETE USER"};
 int status = WL_IDLE_STATUS;
 int choice = 0;
 String uid = "";
 String sPayload;
 char* cPayload;
-
-/*************** TEMPORARY SIMULATION ***************/
-const int deniedButton = 8;
-const int allowedButton = 7;
-/****************************************************/
+bool first = false;
+bool second = false;
 
 //MQTT Credentials
 const char broker[] = MQTT_BROKER;
@@ -56,13 +53,17 @@ void openDoorRequest(String addressCard) {
 
 void openDoor() {
     lcd.clear();
-    lcd.print("Allowed to pass");
+    lcd.setCursor(0, 0);
+    lcd.print("   ALLOWED TO PASS!");
     digitalWrite(ALLOWED_LED, HIGH);
     for(int i = 0; i <= 90; i++) {
       servo.write(i);
     }
     
     delay(3000);
+    lcd.setCursor(0, 0);
+    lcd.print("Please, close the   door :)");
+    while(!digitalRead(CHECK));
     
     for(int i = 90; i >= 0; i--) {
       servo.write(i);
@@ -71,11 +72,11 @@ void openDoor() {
 
 void denyAccess() {
   lcd.clear();
-  lcd.print("Not allowed!");
+  lcd.print("    NOT ALLOWED!");
   Serial.println("Access denied");
   Serial.println();
   digitalWrite(NOT_ALLOWED_LED, HIGH);
-  delay(1000);
+  delay(3000);
 }
 
 void mqttBrokerConnect() {
@@ -83,16 +84,25 @@ void mqttBrokerConnect() {
 
   Serial.print("Attempting to connect to the MQTT broker: ");
   Serial.println(broker);
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Connecting to MQTT  broker");
 
   if (!mqttClient.connect(broker, port)) {
     Serial.print("MQTT connection failed! Error code = ");
     Serial.println(mqttClient.connectError());
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("MQTT connection failed!");
 
     while (1);
   }
 
   Serial.println("You're connected to the MQTT broker!");
   Serial.println();
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Connected to MQTT   broker");
 
   // set the message receive callback
   mqttClient.onMessage(onMqttMessage);
@@ -111,6 +121,7 @@ void mqttBrokerConnect() {
   Serial.print(" - ");
   Serial.println(register_response);
   mqttClient.subscribe(register_response);
+  delay(500);
 }
 
 // On receive message from the topic
@@ -131,6 +142,9 @@ void onMqttMessage(int messageSize) {
   }
   
   Serial.println(options[choice]);
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print(options[choice]);
 } 
 
 void wifiConnect() {
@@ -149,14 +163,23 @@ void wifiConnect() {
   while (status != WL_CONNECTED) {
     Serial.print("Attempting to connect to WPA SSID: ");
     Serial.println(ssid);
+    lcd.clear();
+    lcd.setCursor(0, 0);
+    lcd.print("Connecting to WPA:");
+    lcd.setCursor(0, 1);
+    lcd.print(ssid);
     status = WiFi.begin(ssid, pass);
 
     delay(10000);
   }
 
   Serial.print("You're connected to the network");
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Connected!");
   printCurrentNet();
   printWifiData();
+  delay(500);
 }
 
 void printWifiData() {
@@ -225,10 +248,8 @@ String readCard() {
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
-  
-  Serial.println();
-  Serial.print("Message : ");
   content.toUpperCase();
+  
   return (String) content;
 }
 
@@ -262,6 +283,10 @@ void addCard(String newCard) {
   mqttClient.endMessage();
   
   choice = 0;
+
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print(options[choice]);
 }
 
 void setup() {
@@ -271,15 +296,29 @@ void setup() {
   pinMode(UP, INPUT);
   pinMode(DOWN, INPUT);
 
-  // TEMPORARY SIMULATION
-  pinMode(allowedButton, INPUT);
-  pinMode(deniedButton, INPUT);
-
   lcd.begin();
   lcd.backlight();
   lcd.clear();
-  lcd.print("Waiting for a card...");
+
+  if(!digitalRead(CHECK)) {
+    lcd.setCursor(0, 0);
+    lcd.print("  MAINTENANCE MODE");
+    lcd.setCursor(0, 1);
+    lcd.print("Close the door to");
+    lcd.setCursor(0, 2);
+    lcd.print("begin");
+
+    digitalWrite(ALLOWED_LED, HIGH);
+    digitalWrite(NOT_ALLOWED_LED, HIGH);
+    
+    while(!digitalRead(CHECK));
+  }
+
   servo.attach(2);
+  
+  servo.write(90);
+  delay(1000);
+  
   servo.write(0);
   Serial.begin(9600);  
   while (!Serial);
@@ -288,6 +327,9 @@ void setup() {
   SPI.begin();
   mfrc522.PCD_Init();  
   Serial.println(options[choice]);
+  lcd.clear();
+  lcd.setCursor(0, 1);
+  lcd.print(options[choice]);
 }
 
 void loop() {
@@ -296,36 +338,25 @@ void loop() {
   digitalWrite(ALLOWED_LED, LOW);
   digitalWrite(NOT_ALLOWED_LED, LOW);
 
-  if (digitalRead(UP) == 1) { //INCREASE CHOICE
+  /*if (digitalRead(UP) == 1) { //INCREASE CHOICE
     choice = (choice + 1) % 3;
     Serial.println(options[choice]);
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print(options[choice]);
   } else if(digitalRead(DOWN) == 1) { //DECREASE CHOICE
-    if (choice > 0) {
-      choice = (choice - 1) % 3;
-      Serial.println(options[choice]);
-    }
+    if (choice == 0) choice = 2;
+    else choice--;
+    Serial.println(options[choice]);
+    lcd.clear();
+    lcd.setCursor(0, 1);
+    lcd.print(options[choice]);
   }
 
-  delay(150);
-
-  /*switch(choice) {
-    case 0:
-      Serial.println("Approximate your card to the reader...");
-      Serial.println();
-      break;
-    case 1:
-      Serial.println("Add user card...");
-      Serial.println();
-      break;
-    case 2:
-      Serial.println("Delete user card...");
-      Serial.println();
-      break;
-    default: break;
-  }*/
+  delay(500);*/
 
   // Look for new cards
-  /*if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
   {
     return;
   }
@@ -336,42 +367,21 @@ void loop() {
   }
   //Show UID on serial monitor
   Serial.print("UID tag :");
-  String uid= "";
+  String content= "";
   byte letter;
   
   for (byte i = 0; i < mfrc522.uid.size; i++) {
      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
      Serial.print(mfrc522.uid.uidByte[i], HEX);
-     uid.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
-     uid.concat(String(mfrc522.uid.uidByte[i], HEX));
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
-  uid.toUpperCase();*/
+  
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+  
+  openDoorRequest(content.substring(1));
 
-  /*************** TEMPORARY SIMULATION ***************/
-  if(digitalRead(allowedButton) == 1) {
-    uid = "17 0E 70 1C";
-    Serial.println("Green pressed!");
-  }
-  if(digitalRead(deniedButton) == 1) {
-    uid = "AA BB CC DD";
-    Serial.println("Red pressed!");
-  }
-  /****************************************************/
-
-  if(uid != "") {
-    switch(choice) {
-      case 0:
-        openDoorRequest(uid);
-        break;
-      case 1:
-        addCard(uid);
-        break;
-      case 2:
-        //removeCard(uid);
-        break;
-      default: break;
-    }
-  }
-
-  uid = "";
+  delay(1000);
 }
